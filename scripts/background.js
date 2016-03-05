25f47e55c575cd1d68ef47f0c32050afc88d2154
Lap Shun Yu <yulapshun@oursky.com>
2016-03-05 18:20:40 +0800

    Add content script to change text field

--- scripts/background.js
+++ scripts/background.js
@@ -1,8 +1,12 @@
 'use strict';
 
 chrome.runtime.onMessageExternal.addListener(function (request, sender, sendResponse) {
-  console.log(request);
-});
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, request);
+    });
+    console.log(request);
+  }
+);
 
 chrome.runtime.onInstalled.addListener(function (details) {
   console.log('previousVersion', details.previousVersion);
